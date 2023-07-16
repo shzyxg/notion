74c5846207f61b140b045e7509a978299cc2edc3
SHZYXG <81144955+shzyxg@users.noreply.github.com>
2023-07-16 14:15:40 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,8 +35,11 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
 
+<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fmovie.jiangshuai.com.cn&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a>
+      <div className={styles.copyright}>Copyright 2016-2023 {config.author}</div>
+
+      
       <div className={styles.settings}>
         {hasMounted && (
           <a
