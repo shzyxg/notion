d07d7f9043fd06e9780e6d11411d51af89950c34
SHZYXG <81144955+shzyxg@users.noreply.github.com>
2023-07-16 15:30:26 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/muzhuanggang'
+      href='https://github.com/shzyxg'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
