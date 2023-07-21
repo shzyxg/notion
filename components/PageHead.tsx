4d7d8faeea734567b4c1ba8454f3e5d456d0d7e3
SHZYXG <81144955+shzyxg@users.noreply.github.com>
2023-07-21 13:16:39 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -82,7 +82,7 @@ export const PageHead: React.FC<
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
-
+        <script type="text/javascript" src="https://js.users.51.la/21317675.js"></script>
       <script src="//code.jivosite.com/widget/l2alQ7CldD" async></script>
     </Head>
   )
